b598a050eca82944c7e71854ee664af64ece3287
Jordy Pereira <dev@jordypereira.be>
2021-05-16 14:00:29 +0200

    zoom

--- map.ts
+++ map.ts
@@ -1,7 +1,7 @@
 import L from 'leaflet'
 
 
-var map = L.map('mapid').setView([60.8903488,18.0503477], 14);
+var map = L.map('mapid').setView([60.8903488,18.0503477], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> 1, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
