17d381d1d1c8646a08c0110bba50c7db4a201b98
Jordy Pereira <dev@jordypereira.be>
2021-05-16 13:59:46 +0200

    Add a map

---
+++ map.ts
@@ -0,0 +1,41 @@
+import L from 'leaflet'
+
+
+var map = L.map('mapid').setView([60.8903488,18.0503477], 14);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> 1, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 15,
+  minZoom: 13,
+  id: 'jordypereira/ckor0axk3588317le3ojli8tx',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'pk.eyJ1Ijoiam9yZHlwZXJlaXJhIiwiYSI6ImNrb3IwOWRvdzB5Y3AybnN6cGlnbTVmdzUifQ.JeTpr6XvhG_NK2afZOz0ag'
+}).addTo(map);
+
+setTimeout(() => map.invalidateSize(), 100)
+
+var myIcon = L.icon({
+    iconUrl: '/assets/images/concert.png',
+    iconSize: [38, 52],
+    iconAnchor: [19, 52],
+    popupAnchor: [0, -52],
+});
+
+var marker1 = L.marker([59.316688, 18.033687], {
+  riseOnHover: true,
+  icon: myIcon
+}).addTo(map);
+marker1.bindPopup("Hornhuset<br> Great beer", {autoClose: false}).openPopup();
+
+var marker2 = L.marker([59.318062,18.054688], {
+  riseOnHover: true,
+  icon: myIcon
+}).addTo(map);
+marker2.bindPopup("Häktet<br> Party all night", { autoClose: false }).openPopup();
+
+var marker3 = L.marker([59.314437,18.066313], {
+  riseOnHover: true,
+  icon: myIcon
+}).addTo(map);
+marker3.bindPopup("Hellströms<br> Cheapest beer in town", {autoClose: false}).openPopup();
