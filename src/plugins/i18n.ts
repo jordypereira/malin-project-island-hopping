239b50180d2264fb9c8995aad42adecbcad37531
Jordy Pereira <dev@jordypereira.be>
2023-06-05 21:16:04 +0200

    feat: storable

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,14 +1,14 @@
 import { derived } from 'svelte/store';
-import { writable } from 'svelte/store';
 
 import en from '../locales/en.json';
 import no from '../locales/no.json';
+import { storable } from './storable';
 
 // Initialize the language store with English language pack
-export const language = writable('no');
+export const language = storable('no');
 
 // Function to change the language
-export function changeLanguage(lang) {
+export function changeLanguage (lang) {
   language.set(lang);
 }
 
